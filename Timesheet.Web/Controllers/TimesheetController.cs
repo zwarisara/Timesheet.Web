f1b6179893ab6aceb5c3ebf8d511c973cd4f7bc9
Toei <toei3421@gmail.com>
2020-12-02 11:10:25 +0700

    ปรับ select picker

--- Timesheet.Web/Controllers/TimesheetController.cs
+++ Timesheet.Web/Controllers/TimesheetController.cs
@@ -15,7 +15,7 @@ namespace Timesheet.Web.Controllers
             TimesheetModel model = new TimesheetModel();
             model.DATE_OF = DateTime.Now;
             model.JOB_CODE_LIST = _TimeSheetRepo.GetAllJobCode();
-            model.SUB_JOB_CODE_LIST = _TimeSheetRepo.GetAllSubJobCode("38");
+            model.SUB_JOB_CODE_LIST = _TimeSheetRepo.GetAllSubJobCode();
             return View(model);
         }
 
